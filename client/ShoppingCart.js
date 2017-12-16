1ab20cebd3f973d1c5a824d01df71bf4e5a710fe
lichin shao <lichinshao@gmail.com>
2017-12-16 14:58:50 -0800

    created ShoppingCart component

---
+++ client/ShoppingCart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  render() {
+    return(
+      <div >
+      </div>
+    )
+  }
+}
